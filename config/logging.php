6e3123d3c049dc367649be60c81b41601a7716b4
Patric Vinicios <psdev7@gmail.com>
2026-08-16 13:14:09 -0300

    feat(pokeapi): add client configuration and dedicated log channel

--- config/logging.php
+++ config/logging.php
@@ -65,6 +65,13 @@ return [
             'replace_placeholders' => true,
         ],
 
+        'pokeapi' => [
+            'driver' => 'single',
+            'path' => storage_path('logs/pokeapi.log'),
+            'level' => 'warning',
+            'replace_placeholders' => true,
+        ],
+
         'daily' => [
             'driver' => 'daily',
             'path' => storage_path('logs/laravel.log'),
